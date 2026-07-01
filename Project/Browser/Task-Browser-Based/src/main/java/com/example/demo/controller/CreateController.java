f9357b1551f76828c8a94d549ad8693086cd490d
Amit Krishna <150180020+Amit-Krishna@users.noreply.github.com>
2026-07-01 23:44:27 +0530

    Add files via upload

---
+++ Project/Browser/Task-Browser-Based/src/main/java/com/example/demo/controller/CreateController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.services.MyServices;
+
+@Controller
+public class CreateController {
+	@Autowired
+	private MyServices services;
+	
+	@PostMapping("/addUser")
+	public String addUser(User user)
+	{
+		boolean flag = services.addUser(user);
+
+	    if(flag)
+	        return "addUser";
+
+	    return "Main";
+	}
+	
+	@GetMapping("/getUsers")
+	public String getUsers(Model model)
+	{
+		model.addAttribute("users", services.getUsers());
+		return "users";
+	}
+	
+	@PostMapping("/raiseSalary")
+	public String raiseSalary(@RequestParam int id,@RequestParam double raisePercent,Model model)
+	{
+		if (!services.handleSalaryRaise(id,raisePercent)) {
+	        model.addAttribute("error", "User not found");
+	    }
+		else model.addAttribute("success", "Salary Updated");
+
+	    return "Main";
+	}
+	@GetMapping("/Main")
+	public String showMainPage() {
+		return "Main"; 
+		
+	}
+	@GetMapping("/addUser")
+	public String addUserPage() {
+	    return "addUser";
+	}
+	
+	@GetMapping("/raiseSalary")
+	public String raiseSalaryPage() {
+	    return "raiseSalary";
+	}
+	@GetMapping("/exit")
+	public String exit() {
+	    return "thankYou";   
+	}
+}
---
+++ Project/Browser/Task-Browser-Based/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,60 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String name; 
+	private int age;
+	private String designation;
+	private double salary;
+	private boolean bool;
+	
+	public boolean getBool() {
+		return bool;
+	}
+	public void setBool(Boolean bool) {
+		this.bool = bool;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double d) {
+		this.salary = d;
+	}
+	
+
+}
